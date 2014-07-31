3cb2fedb310242be211786ac7fe257fa99501260
bartimaeusx <stevennesmith@gmail.com>
2014-07-31 11:54:29 -0400

    added simulation mode external variable
    for detecting if rider wants simulation mode

--- Aerovelo/HPV_ArduinoMega/TargetSpeed_Func.ino
+++ Aerovelo/HPV_ArduinoMega/TargetSpeed_Func.ino
@@ -1,107 +1,112 @@
 /* Target Speed Functions */
 
+extern bool simulate_mode;
+
 /*
 Updates profileNum, profileFilename, logFilename and starting GPS location if the yellow button is pressed
  */
 void toggle(){
   
-  File dataFile;
+	File dataFile;
   
-  static char _word[32] = {0};
-  //Read button
-  Toggle = !digitalRead(8);
-  //If button was pressed, update "profile"
-  if (Toggle != lastToggle){
-    Serial.println('Toggle!');
+	static char _word[32] = {0};
+	//Read button
+	Toggle = !digitalRead(8);
+	//If button was pressed, update "profile"
+	if (Toggle != lastToggle){
+		Serial.println('Toggle!');
 
-    lastToggle = Toggle;
+		lastToggle = Toggle;
 
-    // Update profileNum
-    profileNum++;
+		// Update profileNum
+		profileNum++;
 
-    if (profileNum > MAX_PROFILE_NUM) 
-    {
-      profileNum = 0;
-    }
+		if (profileNum > (MAX_PROFILE_NUM + 1)) 
+		{
+			profileNum = 0;
+		}
 
-    Serial.print("Updated Profile #: ");
-    Serial.println(profileNum);
-    // Update profileFilename based on profileNum; 0 -> PAUSED
-    if (profileNum != 0){
-      // Figure out SD card log iteration.
-      sprintf(logFilename, "LG%02u%02u%02u.txt", GPS.UTC.hour, GPS.UTC.day, GPS.UTC.month);//, GPS.UTC.hour, GPS.UTC.minute, GPS.UTC.second);
-      sprintf(profileFilename, "PF%02d.txt", profileNum);
-      //sprintf(profileFilename, "LALALA");
-      Serial.println(profileFilename);
-      Serial.println(logFilename);
+		if (profileNum <= MAX_PROFILE_NUM) {
+			simulation_mode = false;
+			Serial.print("Updated Profile #: ");
+			Serial.println(profileNum);
+			// Update profileFilename based on profileNum; 0 -> PAUSED
+			if (profileNum != 0){
+				// Figure out SD card log iteration.
+				sprintf(logFilename, "LG%02u%02u%02u.txt", GPS.UTC.hour, GPS.UTC.day, GPS.UTC.month);//, GPS.UTC.hour, GPS.UTC.minute, GPS.UTC.second);
+				sprintf(profileFilename, "PF%02d.txt", profileNum);
+				//sprintf(profileFilename, "LALALA");
+				Serial.println(profileFilename);
+				Serial.println(logFilename);
 
-      dataFile = SD.open(profileFilename, FILE_READ);
-      if (dataFile) {
-        Serial.println("File opened");
-        int n = sd_ReadWord(dataFile, _word);
-        memcpy(profileName, _word, n);
-        profileName[n] = 0;
-        Serial.println(profileName);
+				dataFile = SD.open(profileFilename, FILE_READ);
+				if (dataFile) {
+					Serial.println("File opened");
+					int n = sd_ReadWord(dataFile, _word);
+					memcpy(profileName, _word, n);
+					profileName[n] = 0;
+					Serial.println(profileName);
 
-        Serial.print("Coefficients: ");
-        for (int i = 0; i < 7; ++i){
-          sd_ReadWord(dataFile, _word);
-          coeff[i] = atof(_word);
-          Serial.println(coeff[i]);
+					Serial.print("Coefficients: ");
+					for (int i = 0; i < 7; ++i){
+						sd_ReadWord(dataFile, _word);
+						coeff[i] = atof(_word);
+						Serial.println(coeff[i]);
+					} 
 
-        } 
+					dataFile.close();
+				} else {
+					Serial.println("SD Read Error (Coeffs)");
+				}
 
-        dataFile.close();
-      } 
-      else {
-        Serial.println("SD Read Error (Coeffs)");
-      }
-
-      // Check accuracy first?
+				// Check accuracy first?
       
-      dataFile = SD.open("gpsRef.txt", FILE_READ);
+				dataFile = SD.open("gpsRef.txt", FILE_READ);
       
-      sd_ReadWord(dataFile, _word);
-      LattitudeStart = atof(_word)*10000000;
-      sd_ReadWord(dataFile, _word);  
-      LongitudeStart = atof(_word)*10000000;
-      //AltidudeStart = GPS.Altitude;
-      dataFile.close();
-    } else {
+				sd_ReadWord(dataFile, _word);
+				LattitudeStart = atof(_word)*10000000;
+				sd_ReadWord(dataFile, _word);  
+				LongitudeStart = atof(_word)*10000000;
+				//AltidudeStart = GPS.Altitude;
+				dataFile.close();
+			} else {
 
-      sprintf(logFilename, "LGdflt.txt");
+				sprintf(logFilename, "LGdflt.txt");
 
-      dataFile = SD.open("PFdflt.txt", FILE_READ);
-      if (dataFile) {
-        Serial.println("File opened");
-        int n = sd_ReadWord(dataFile, _word);
-        memcpy(profileName, _word, n);
-        Serial.println(profileName);
+				dataFile = SD.open("PFdflt.txt", FILE_READ);
+				if (dataFile) {
+					Serial.println("File opened");
+					int n = sd_ReadWord(dataFile, _word);
+					memcpy(profileName, _word, n);
+					Serial.println(profileName);
 
-        Serial.print("Coefficients: ");
-        for (int i = 0; i < 7; ++i){
-          sd_ReadWord(dataFile, _word);
-          coeff[i] = atof(_word);
-          Serial.println(coeff[i]);
+					Serial.print("Coefficients: ");
+					for (int i = 0; i < 7; ++i){
+						sd_ReadWord(dataFile, _word);
+						coeff[i] = atof(_word);
+						Serial.println(coeff[i]);
+					} 
 
-        } 
-
-      dataFile.close();
-      }
-      
+					dataFile.close();
+				}
       
       
-      dataFile = SD.open("gpsTest.txt", FILE_READ);
-      if (dataFile) {
-        sd_ReadWord(dataFile, _word);
-        LattitudeStart = atof(_word)*10000000;
-        sd_ReadWord(dataFile, _word);  
-        LongitudeStart = atof(_word)*10000000;
-        //AltidudeStart = GPS.Altitude;
-        dataFile.close();
-      }
-    }
-  }
+      
+				dataFile = SD.open("gpsTest.txt", FILE_READ);
+				if (dataFile) {
+					sd_ReadWord(dataFile, _word);
+					LattitudeStart = atof(_word)*10000000;
+					sd_ReadWord(dataFile, _word);  
+					LongitudeStart = atof(_word)*10000000;
+					//AltidudeStart = GPS.Altitude;
+					dataFile.close();
+				}
+			}
+		} else { // profileNum == MAX_PROFILE_NUM+1 (simulate mode)
+			Serial.println("Simulation Mode Entered");
+			simulation_mode = true;	
+		}
+	}
 }
 
 /*
