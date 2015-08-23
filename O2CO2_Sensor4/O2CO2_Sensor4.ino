a35405566046e34b0bef438359a901398b126183
ASelwa <selwa.alex@gmail.com>
2015-08-23 19:07:14 -0400

    Updated CO2 O2 Temp

--- O2CO2_Sensor4/O2CO2_Sensor4.ino
+++ O2CO2_Sensor4/O2CO2_Sensor4.ino
@@ -7,24 +7,252 @@
  Parts required:
  O2 Sensor - Figaro SK-25F
  CO2 Sensor - Figaro TGS 4161
- 
  */
+ 
+// Set up infrared
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
-// include the math library
+
+
+// Set up thermocouple
+#include <SPI.h>
+#include "Adafruit_MAX31855.h" 
+// Example creating a thermocouple instance with software SPI on any three
+// digital IO pins.
+#define DO   3
+#define CS   4
+#define CLK  5
+Adafruit_MAX31855 thermocouple(CLK, CS, DO);
+ 
+// Set up O2 and CO2 sensor
 #include <math.h>
 #include <SoftwareSerial.h>
-//
 int const pinO2 = A0; // analog pin used to connect the O2 sensor
-
 const int pinRx = 8;
 const int pinTx = 7;
 SoftwareSerial K_30_Serial(pinTx,pinRx);
-unsigned char flg_get = 0;              // if get sensor data
 int valMultiplier = 1; 
-
 byte readCO2[] = 
   {0xFE, 0X44, 0X00, 0X08, 0X02, 0X9F, 0X25};
 byte response[] = {0,0,0,0,0,0,0};  //create an array to store the response 
+
+////Declare variables used in loops
+float meanO2 = 0;
+float gainO2 = 100;
+float meanVoltO2 = 0;
+float baseVoltO2 = 0;
+int valO2 = 0;
+float valVoltO2 = 0;
+float valPercentO2 = 0;
+float CO2ppm = 0;
+float number_of_points = 10; //for initial average
+int threshold = 10;
+float temp_value = 0;
+
+// ADJUST THESE PARAMETERS 
+
+      // How long to collect data for
+      int n_seconds_saved = 3600;
+      int n_average = 4; // 90 gives roughly 0.33 Hz sampling frequency
+    
+      #define SAVE_O2 false
+      #define SAVE_CO2 false
+      #define SAVE_CALIPER true
+      #define SAVE_DISK true
+      #define SAVE_DISK_AMB true
+      #define SAVE_TIME false
+      // DATA_SIZE = 660 / # variables
+      #define DATA_SIZE 180
+      int data_size = DATA_SIZE;
+      float O2_temp;
+      float CO2_temp;
+      float caliper_temp;
+      float disk_temp;
+      float disk_amb_temp;
+      
+      // Set the size as 1 if "SAVE" flags are false 
+      int O2_average[1];
+      int CO2_average[1];
+      unsigned int caliper_average[DATA_SIZE];
+      unsigned int disk_average[DATA_SIZE];
+      unsigned int disk_amb_average[DATA_SIZE];
+      int time[1];
+      
+      int datapoint = 0;
+      unsigned long timeOffset;
+      unsigned long totalTime;
+      
+      unsigned long valCO2 = 0;
+
+      // For printing data as it's collected
+      #define PRINT_FLAG false
+
+
+void setup(){
+  Serial.begin(115200);
+  K_30_Serial.begin(9600);//start the reading of the co2 sensor
+
+  // Calibrate O2 Sensor
+  if (SAVE_O2) {
+    for (int i=0; i < number_of_points; i++){
+      meanO2 = meanO2 + analogRead(pinO2);
+      //
+      // print out the values to the serial monitor
+      Serial.print("Calibration Point: ");
+      Serial.print(i);
+      Serial.print("  ");
+      Serial.print("meanO2: ");
+      Serial.println(analogRead(pinO2));
+      delay(1000);
+    //
+    } 
+    meanO2 = meanO2/number_of_points;
+    
+    Serial.print("Final meanO2: ");
+    Serial.println(meanO2);
+  }
+  
+  // IR temperature sensor setup
+  mlx.begin();
+
+  timeOffset = millis(); // To track the starting time  
+}
+
+// Measurement and Printout Loop
+void loop(){
+  
+  // Reset these "accumulator" variables
+  O2_temp = 0;
+  CO2_temp = 0;
+  caliper_temp = 0;
+  disk_temp = 0;
+  disk_amb_temp = 0;
+  
+  for (int i = 0; i<n_average; i++){
+    
+    if (SAVE_O2) {
+      valO2 = analogRead(pinO2); // read the value of the O2 sensor
+      O2_temp = O2_temp + valO2;
+    }
+    if (SAVE_CO2) {
+        sendRequest(readCO2); 
+      if (getValue(response) < 10001)
+      {
+        valCO2 = getValue(response); 
+      }
+      else
+      {
+        valCO2 = 10000; 
+        //valCO2 = CO2_average[datapoint]/i; // Divide by zero?
+      }
+      CO2_temp = CO2_temp + valCO2;      
+    }
+    if (SAVE_CALIPER) {
+      caliper_temp = caliper_temp + thermocouple.readCelsius();
+    }
+    if (SAVE_DISK) {
+          disk_temp = disk_temp + mlx.readObjectTempC();
+    }
+    if (SAVE_DISK_AMB) {
+          disk_amb_temp = disk_amb_temp + mlx.readAmbientTempC();
+    }
+      
+    if (PRINT_FLAG) {
+      Serial.print(" , O2 = ");
+      Serial.println(valO2);
+      Serial.print(" , CO2 ppm = ");
+      Serial.println(valCO2);
+      Serial.print("Time (ms): ");
+      Serial.println(millis()-timeOffset);
+    }
+  }
+  
+  // Store the data in the array
+  if (SAVE_O2) {
+    O2_average[datapoint] = O2_temp/n_average;
+  }
+  if (SAVE_CO2) {
+    CO2_average[datapoint] = CO2_temp/n_average;
+  }
+  if (SAVE_CALIPER) {
+    caliper_average[datapoint] = caliper_temp/n_average*100;
+  }
+  if (SAVE_DISK) {
+    disk_average[datapoint] = disk_temp/n_average*100;
+  }
+  if (SAVE_DISK_AMB) {
+    disk_amb_average[datapoint] = disk_amb_temp/n_average*100;
+  }  
+  if (SAVE_TIME) {
+    time[datapoint] = (millis()-timeOffset)/1000;
+  }
+
+  // Log to Serial Monitor  
+  if (Serial.available() > 0) { 
+    totalTime = millis()-timeOffset; // Save the end time
+    print_function(true);
+  }
+  datapoint++;
+  
+  // STAY IN THIS PRINTING LOOP FOREVER WHEN RUN OUT ROOM IN THE DATA ARRAYS
+  if (datapoint > data_size-1) {
+    datapoint--;
+    totalTime = millis()-timeOffset; // Save the end time
+    while (true) {
+      print_function(true);
+      delay(10000); // It will print the entire log every 10 seconds
+    } 
+  }    
+}
+
+
+/**************************************************************
+ *                      FUNCTIONS                             *
+ **************************************************************/
+void print_function(bool finished) {
+      Serial.println("***************** DATA *****************");
+      if (SAVE_O2) {
+        Serial.print("Initial O2 reading : ");
+        Serial.println(meanO2);  
+      }
+      for (int j = 0; j <datapoint; j++) // print all the datapoints so far
+      {   
+        Serial.print(j+1); 
+        if (SAVE_O2) {
+          Serial.print(", O2 Level [%]: ");
+          Serial.print(O2_average[j]/meanO2*100);
+        }
+        if (SAVE_CO2) {       
+          Serial.print(", CO2 Level [ppm]: ");
+          Serial.print(CO2_average[j]);
+        }
+        if (SAVE_CALIPER) {
+          Serial.print(", Caliper [deg C]: ");
+          Serial.print((float)caliper_average[j]/100);
+        }
+        if (SAVE_DISK) {
+          Serial.print(", Disk [deg C]: ");
+          Serial.print((float)disk_average[j]/100);
+        }
+        if (SAVE_DISK_AMB) {
+          Serial.print(", Disk Ambient [deg C]: ");
+          Serial.print((float)disk_amb_average[j]/100);
+        } 
+        if (SAVE_TIME) {
+          Serial.print(",  Time [s]: ");
+          Serial.print(time[j]);
+        }
+        Serial.println(" ");
+      }
+      if (finished) {
+          Serial.print("Total Time Logged [s]: ");
+          Serial.println(totalTime/1000);
+        }
+      Serial.println("*************** END DATA ***************");
+}
+
 
 //function for reading the UART co2 sensor
 void sendRequest(byte packet[]) 
@@ -35,7 +263,7 @@ void sendRequest(byte packet[])
     delay(25); 
   } 
    
-  int timeout=0;  //set a timeoute counter 
+  int timeout=0;  //set a timeout counter 
   while(K_30_Serial.available() < 7 )    //Wait to get a 7 byte response 
   { 
     timeout++;   
@@ -44,6 +272,7 @@ void sendRequest(byte packet[])
         while(K_30_Serial.available())  //flush whatever we have 
           K_30_Serial.read(); 
            
+          timeout = 0;   
           break;                        //exit and try again 
       } 
       delay(25); 
@@ -64,117 +293,4 @@ unsigned long getValue(byte packet[])
     unsigned long val = high*256 + low;                //Combine high byte and low byte with this formula to get value 
     return val* valMultiplier; 
 } 
-
-////Declare variables used in loops
-float meanO2 = 0;
-
-
-float gainO2 = 100;
-float meanVoltO2 = 0;
-float baseVoltO2 = 0;
-
-
-int valO2 = 0;
-
-float valVoltO2 = 0;
-float valPercentO2 = 0;
-float CO2ppm = 0;
-
-float number_of_points = 10; //for initial average
-int threshold = 10;
-float temp_value = 0;
-//float change = 100;
-int seconds_per_datapoint = 5;
-int n_average = 18*seconds_per_datapoint;//this should give us 30 seconds per datapoint 
-float O2_average[100];
-float CO2_average[100];
-int datapoint = 0;
-
-void setup(){
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(115200);
-  K_30_Serial.begin(9600);//start the reading of the co2 sensor
-//  while (change > threshold){// wait for the O2 sensor to reach steady state value
-//    temp_value = analogRead(pinO2);
-//    delay(1000);
-//    change = analogRead(pinO2) - temp_value;
-//    Serial.print("waiting for O2 sensor...");
-//    Serial.println(change);
-//  }
-  //  // Calibrate O2 Sensor 
-  //
-  for (int i=0; i < number_of_points; i++){
-    meanO2 = meanO2 + analogRead(pinO2);
-    //
-    // print out the values to the serial monitor
-    Serial.print("Calibration Point: ");
-    Serial.print(i);
-    Serial.print("  ");
-    Serial.print("meanO2: ");
-    Serial.println(analogRead(pinO2));
-    delay(1000);
-  //
-  } 
-  meanO2 = meanO2/number_of_points;
-  
-  Serial.print("Final meanO2: ");
-  Serial.println(meanO2);
-  //
-  // Assume typical atmospheric concentration of O2 (20.95%), determine 0% O2 Voltage
-//  meanVoltO2 = map(meanO2, 0, 1023, 0, 500);
-//  baseVoltO2 = (meanVoltO2/100/gainO2);//-0.0067319;
-  Serial.print("Value outside O2: ");
-  Serial.println(meanO2);
-}
-
-
-// Measurement and Printout Loop
-void loop(){
-  O2_average[datapoint] = 0;
-  CO2_average[datapoint] = 0;
-  //  
-  for (int i = 0; i<n_average; i++){
-    valO2 = analogRead(pinO2); // read the value of the O2 sensor
-    valPercentO2 = valO2/meanO2*100;
-    delay(5);
-    // read the value of the CO2 sensor. this is done directly using the uart
-    Serial.println("********************************************************");// DO NOT COMMENT OUT
-    Serial.println(); // THIS LINE AND THE ONE ABOVE SEEM TO BE CRITICAL TO GET THE CO2 SENSOR WORKINg...
-    flg_get = 0;
-    int gas, temp;
-    sendRequest(readCO2); 
-    unsigned long valCO2 = 0;
-    if (getValue(response) < 10001)
-    {
-      valCO2 = getValue(response); 
-    }
-    else
-    {
-      valCO2 = CO2_average[datapoint]/i;
-    }
-    Serial.print(" , O2 percentage = ");
-    Serial.println(valPercentO2);
-  Serial.print(" , CO2 ppm = ");
-    Serial.println(valCO2);
-    O2_average[datapoint] = O2_average[datapoint] + valPercentO2;
-    CO2_average[datapoint] = CO2_average[datapoint] + valCO2;
-    delay(10);//got to figure out the actual timing here so we can match the duration of the run to the data
-  } 
-  O2_average[datapoint] = O2_average[datapoint]/n_average;
-  CO2_average[datapoint] = CO2_average[datapoint]/n_average;
-  Serial.println("***************** DATA ************");
-  Serial.print("initial O2 reading : ");
-  Serial.println(meanO2);  
-  for (int j = 0; j <datapoint; j++) // print all the datapoints so far
-  {   
-     Serial.print("O2 Level [%]: ");
-     Serial.print(O2_average[j]);
-     Serial.print(", CO2 Level [ppm]: ");
-     Serial.println(CO2_average[j]);
-  }
-  Serial.println("*********** END DATA ************");
-  datapoint++;
-  //
-}
-
 
